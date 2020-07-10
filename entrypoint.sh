47e427a252d3d21688dd8651841ee60475c1a890
Poulinho <poulai_a@etna-alternance.net>
2020-07-10 15:22:01 +0200

    remove old content

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Calling validator..."
-zipPath="@$1;type=application/zip"
-pwd
-ls
-curl -X POST -F archive=@$zipPath -F key=$2  https://validator.prestashop.com/api/modules
